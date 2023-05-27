f5a04b7a19548f6ef0da4bf847489b2cfd3b32da
mellyexe <125124784+mellyexe@users.noreply.github.com>
2023-05-27 15:24:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,33 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  std::ifstream file(filename);
+  BST<std::string> tree;
+  std::string word;
+
+  if (file) {
+    while (!file.eof()) {
+      word.erase();
+      while (1) {
+        char symbol = file.get();
+        if (symbol > 96 && symbol < 123) {
+          word += symbol;
+        } else if (symbol > 64 && symbol < 91) {
+            symbol += 32;
+            word += symbol;
+        } else {
+          break;
+        }
+      }
+      if (!word.empty()) {
+        tree.addNode(word);
+      } else {
+        continue;
+      }
+    }
+    file.close();
+  } else {
+    std::cout << "File was not read correctly." << std::endl;
+  }
+  return tree;
 }
